ab3f06d970e1eddb5c8edf92cc8367610e4c4906
nir85 <88407207+nir85@users.noreply.github.com>
2021-10-14 15:21:20 +0200

    Update main.js

--- openlayers_rad/main.js
+++ openlayers_rad/main.js
@@ -138,10 +138,5 @@ function init(){
 			enableHighAccuracy: false,
   		}
 	});
-		console.log(geolocation);
-	const coordinates = geolocation.getPosition();
-	console.log("qsdqsd");
-	console.log(coordinates ? new ol.geom.Point(coordinates));
-		    
-	console.log("qsdqsd");
+	console.log(geolocation);
 }
